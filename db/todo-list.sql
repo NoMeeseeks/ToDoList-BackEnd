562564ff3ee1cfa2794fec990f7dc2662a8bd43f
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-07-25 22:53:34 -0500

    creacion 1 parte de la base de datos

---
+++ db/todo-list.sql
@@ -0,0 +1,40 @@
+create table usuarios(
+ 	id_usuario serial primary key,
+ 	nombre varchar(100) not null,
+  	apellido varchar(100) null,
+   	correo varchar(150) not null,
+   	es_activo varchar(1) not null
+);
+
+create table tarea(
+	id_tarea serial primary key,
+	id_prioridad int not null,
+ 	titulo varchar(250) not null,
+  	descripcion varchar(500) null,
+   	fecha date  null,
+   	es_activo varchar(1) not null,
+   	usuario_creacion varchar(100) not null,
+	fecha_creacion date not null,
+	usuario_modificacion varchar(100) null,
+	fecha_modificacion date null
+)
+
+create table prioridades(
+	id_prioridad serial primary key,
+	nombre varchar(100) not null,
+	descripcion varchar(250) null,
+	es_activo varchar(1) not null,
+	usuario_creacion varchar(100) not null,
+	fecha_creacion date not null,
+	usuario_modificacion varchar(100) null,
+	fecha_modificacion date null
+  )
+  
+select *
+from usuarios u ;
+
+select *
+from tarea;
+
+select *
+from prioridades;
