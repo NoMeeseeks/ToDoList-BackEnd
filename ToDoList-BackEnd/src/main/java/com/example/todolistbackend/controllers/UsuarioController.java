26f9cd0fc222f588169f4a3f82031ab5af1f858c
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-07-28 22:10:41 -0500

    creacion de los end points
    falta la programar el objeto que devuelve el api

--- ToDoList-BackEnd/src/main/java/com/example/todolistbackend/controllers/UsuarioController.java
+++ ToDoList-BackEnd/src/main/java/com/example/todolistbackend/controllers/UsuarioController.java
@@ -1,13 +1,11 @@
 package com.example.todolistbackend.controllers;
 
+import com.example.todolistbackend.Dto.UsuarioDTO;
 import com.example.todolistbackend.Model.Usuario;
-import com.example.todolistbackend.Repo.IUsuarioRepo;
 import com.example.todolistbackend.services.IUsuarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,6 +26,24 @@ public class UsuarioController {
             System.out.println(item.getNombre());
             System.out.println(item.getApellido());
         }
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/{codigoUsuario}/s")
+    public ResponseEntity<?> actualizarUsuario(
+            @PathVariable(name = "codigoUsuario")Integer codigoUsuario,
+            @RequestBody UsuarioDTO usuarioDTO){
+
+        iUsuarioService.editarUsuarioXIdUsuario(codigoUsuario,usuarioDTO);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{codigoUsuario}")
+    public ResponseEntity<?> eliminarUsuario(@PathVariable(name = "codigoUsuario")Integer codigoUsuario ){
+
+        iUsuarioService.eliminarUsuarioXIdUsuario(codigoUsuario);
 
         return ResponseEntity.ok().build();
     }
